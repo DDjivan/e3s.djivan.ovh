c7bb4fca477d5218584a07ede1d083f388cd9bf0
δDD <64313188+DDjivan@users.noreply.github.com>
2025-09-09 09:29:55 +0200

    Update website_app.py

--- code/website_app.py
+++ code/website_app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from argparse import ArgumentParser
 
 from flask_routes.docs_app import app_docs
 
@@ -13,6 +14,18 @@ app.register_blueprint(app_docs)
 ##————————————————————————————————————————————————————————————————————————————##
  
 if (__name__ == '__main__'):
-    app.run(host='0.0.0.0', debug=True)
+    web_parser = ArgumentParser(description='Deploy app.')
+    web_parser.add_argument(
+        '-d', '--debug', #type=?, choices=?, required=False,
+        # default=False,
+        action='store_true',
+        help="Enable debug mode.",
+        )
+
+    them_args = web_parser.parse_args()
+    # print(them_args)
+
+    # app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=them_args.debug)
 
 
